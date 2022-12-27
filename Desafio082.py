b1f2123b7d2abb00551bf6c6ff297e8d1e24524d
Gianni Gaspari <gianni.gaspari.s@gmail.com>
2022-12-26 23:04:15 -0300

    Atualização

---
+++ Desafio082.py
@@ -0,0 +1,7 @@
+"""
+Desafio 082:
+Crie um programa que vai ler vários números e colocar numa lista.
+Depois disso, crie duas listas extras que vão contar apenas os valores pares
+e os valores impares digitados, respectivamente.
+Ao final, mostre o conteúdo das 3 listas.
+"""
